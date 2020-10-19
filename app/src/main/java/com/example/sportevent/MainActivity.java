76f52de77d4e3d5e57f3ffe57dc516f9d7678bad
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-10-19 18:52:09 +0200

    Show joined event list on the screen

--- app/src/main/java/com/example/sportevent/MainActivity.java
+++ app/src/main/java/com/example/sportevent/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.sportevent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-// Devol
+
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<JoinedEventItem> joinedEventList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            joinedEventList.add(new JoinedEventItem(R.drawable.ic_launcher_foreground, "Event Name: " + i, "Description"));
+        }
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new JoinedEventAdapter(joinedEventList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
 
