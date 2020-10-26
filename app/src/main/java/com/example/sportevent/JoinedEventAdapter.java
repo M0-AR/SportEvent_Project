b806f9bd446c34eaf3f2fad1a4deb9362ef4d073
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-10-26 17:10:39 +0100

    Print the position of item in JoinedEventFragment

--- app/src/main/java/com/example/sportevent/JoinedEventAdapter.java
+++ app/src/main/java/com/example/sportevent/JoinedEventAdapter.java
@@ -13,8 +13,14 @@ import java.util.ArrayList;
 
 public class JoinedEventAdapter extends RecyclerView.Adapter<JoinedEventAdapter.JoinedEventViewHolder> {
     private ArrayList<JoinedEventItem> mJoinedEventList;
+    private static ClickListener mClickListener;
 
-    public static class JoinedEventViewHolder extends RecyclerView.ViewHolder {
+    public interface ClickListener {
+        // Todo why this suggest safe delete
+        void mClick(View view, int position);
+    }
+
+    public static class JoinedEventViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public ImageView mImageView;
         public TextView mTextView1;
         public TextView mTextView2;
@@ -24,11 +30,23 @@ public class JoinedEventAdapter extends RecyclerView.Adapter<JoinedEventAdapter.
             this.mImageView = itemView.findViewById(R.id.imageView);
             this.mTextView1 = itemView.findViewById(R.id.textView1);
             this.mTextView2 = itemView.findViewById(R.id.textView2);
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            mClickListener.mClick(v, getAdapterPosition());
+           // Toast.makeText(v.getContext(), "Event Name: " + getAdapterPosition(), Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
     }
 
-    public JoinedEventAdapter(ArrayList<JoinedEventItem> joinedEventList) {
+    public JoinedEventAdapter(ArrayList<JoinedEventItem> joinedEventList, ClickListener clickListener) {
         this.mJoinedEventList = joinedEventList;
+        mClickListener = clickListener;
     }
 
 
@@ -41,7 +59,7 @@ public class JoinedEventAdapter extends RecyclerView.Adapter<JoinedEventAdapter.
     }
 
     @Override
-    public void onBindViewHolder(@NonNull JoinedEventViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull JoinedEventViewHolder holder, final int position) {
         JoinedEventItem currentItem =  mJoinedEventList.get(position);
 
         holder.mImageView.setImageResource(currentItem.getImageResource());
