e17f1c7780c5312ee5835518b2fb361aa2cf4820
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-10-27 13:03:27 +0100

    - Event's description almost done  - Make a few changes  - Tried to find better solution for adapter

--- app/src/main/java/com/example/sportevent/Event.java
+++ app/src/main/java/com/example/sportevent/Event.java
@@ -1,11 +1,11 @@
 package com.example.sportevent;
 
-public class JoinedEventItem {
+public class Event {
     private int imageResource;
     private String textView1;
     private String textView2;
 
-    public JoinedEventItem(int imageResource, String textView1, String textView2) {
+    public Event(int imageResource, String textView1, String textView2) {
         this.imageResource = imageResource;
         this.textView1 = textView1;
         this.textView2 = textView2;
