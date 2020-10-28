a20ceac07e7d415a12626f051ac10cd63f412134
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-10-28 10:31:06 +0100

    - Create EventStartFragment and add maps dependency  - Make few changes

--- app/src/main/java/com/example/sportevent/fragment/EventResultFragment.java
+++ app/src/main/java/com/example/sportevent/fragment/EventResultFragment.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +13,17 @@ import android.view.ViewGroup;
 
 import com.example.sportevent.R;
 
-public class StartEventFragment extends Fragment {
+public class EventResultFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_start_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_result_event, container, false);
+
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?saddr=20.344,34.34&daddr=20.5666,45.345"));
+        startActivity(intent);
+
         return view;
     }
 }
