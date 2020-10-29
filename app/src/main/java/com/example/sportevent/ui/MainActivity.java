8bce549fa10ee4849076e98cf2cea8293a2d4fbe
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-10-29 16:10:39 +0100

    - Clean up

--- app/src/main/java/com/example/sportevent/ui/MainActivity.java
+++ app/src/main/java/com/example/sportevent/ui/MainActivity.java
@@ -6,26 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
-
-import com.example.sportevent.R;
-
 import com.example.sportevent.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-/*
-Button bNavBottomHome = findViewById(R.id.homeFragment);
-        bNavBottomHome.setOnClickListener(this);
-
-        Button bNavBottomBack = findViewById(R.id.backFragment);
-        bNavBottomBack.setOnClickListener(this);
-*/
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
