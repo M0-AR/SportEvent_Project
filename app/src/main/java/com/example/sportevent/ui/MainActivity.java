f5079a703e6da1d3fa29ec04080f37f48537ec32
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-10-29 16:09:57 +0100

    - Navigation bar can lesson now to any click

--- app/src/main/java/com/example/sportevent/ui/MainActivity.java
+++ app/src/main/java/com/example/sportevent/ui/MainActivity.java
@@ -1,25 +1,43 @@
 package com.example.sportevent.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
-import com.example.sportevent.R;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.sportevent.R;
 
+import com.example.sportevent.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+/*
+Button bNavBottomHome = findViewById(R.id.homeFragment);
+        bNavBottomHome.setOnClickListener(this);
 
-public class MainActivity extends AppCompatActivity {
+        Button bNavBottomBack = findViewById(R.id.backFragment);
+        bNavBottomBack.setOnClickListener(this);
+*/
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -40,6 +58,23 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+       // NavController navController = Navigation.findNavController(getParent().finish());
+        switch (item.getItemId()) {
+            case R.id.homeFragment:
+          //      navController.navigate(R.id.action_eventDescriptionFragment_to_participant);
+                Toast.makeText(MainActivity.this, "Will work next time", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.backFragment:
+                super.onBackPressed();
+                break;
+        }
+        return true;
     }
 }
 
