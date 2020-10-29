17bddb1eeebdf6aea1fc41e6e850b9c9de601ea5
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-10-29 12:05:28 +0100

    - EventStart and EventResult are finished  - Made few changes

--- app/src/main/java/com/example/sportevent/fragment/EventStartFragment.java
+++ app/src/main/java/com/example/sportevent/fragment/EventStartFragment.java
@@ -6,10 +6,14 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.sportevent.Constants;
 import com.example.sportevent.R;
@@ -22,16 +26,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class EventStartFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener{
 
     private MapView mMapView;
+    private Button startEvent;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_start_event, container, false);
 
+        startEvent = view.findViewById(R.id.start_event);
+        startEvent.setOnClickListener(this);
+
         mMapView = view.findViewById(R.id.start_map);
-
         initGoogleMap(savedInstanceState);
-
 
         return view;
     }
@@ -39,29 +45,27 @@ public class EventStartFragment extends Fragment implements OnMapReadyCallback, 
 
 
     private void initGoogleMap(Bundle savedInstanceState){
-        // *** IMPORTANT ***
-        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
-        // objects or sub-Bundles.
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(Constants.MAP_VIEW_BUNDLE_KEY);
         }
-
         mMapView.onCreate(mapViewBundle);
-
         mMapView.getMapAsync(this);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onClick(View v) {
-        String s = v.getTextAlignment()+"";
-
         NavController navController = Navigation.findNavController(v);
-
-        switch (v.getId()) {
-            case R.id.start_event:
-                break;
+        String buttonText = startEvent.getText()+"";
+        if (buttonText.equalsIgnoreCase("Start Event")) {
+            startEvent.setText("Finish Event");
+            // Todo Try to find better solution
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/dir/Copenhagen/Spain/@47.7297451,-4.5837011,5z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x4652533c5c803d23:0x4dd7edde69467b8!2m2!1d12.5683372!2d55.6760968!1m5!1m1!1s0xc42e3783261bc8b:0xa6ec2c940768a3ec!2m2!1d-3.74922!2d40.463667!3e1"));
+            startActivity(i);
         }
+        else
+            navController.navigate(R.id.action_startEvent_to_eventResultFragment);
     }
 
     @Override
