b14013faf05927b860e1687967d386f96cd39dec
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-11-25 20:16:39 +0100

    - Share result's button: sending simple data to other apps

--- app/src/main/java/com/example/sportevent/fragment/EventResultFragment.java
+++ app/src/main/java/com/example/sportevent/fragment/EventResultFragment.java
@@ -10,17 +10,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.sportevent.R;
 
-public class EventResultFragment extends Fragment {
+public class EventResultFragment extends Fragment implements  View.OnClickListener{
+    private Button mShareResult;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_result_event, container, false);
-
-
+        mShareResult = view.findViewById(R.id.share_result);
+        mShareResult.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.share_result) {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Sport Event: The result would be here next time.");
+            sendIntent.setType("text/plain");
+
+            Intent shareIntent = Intent.createChooser(sendIntent, null);
+            startActivity(shareIntent);
+        }
     }
 }
