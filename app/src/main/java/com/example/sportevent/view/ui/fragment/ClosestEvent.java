27403dd805f157c4ec9b55a3dc9e591d64902f4e
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-01-05 12:16:50 +0100

    - ClosestEvent button in bottomNavigationBar is working

---
+++ app/src/main/java/com/example/sportevent/view/ui/fragment/ClosestEvent.java
@@ -0,0 +1,70 @@
+package com.example.sportevent.view.ui.fragment;
+
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.bumptech.glide.Glide;
+import com.example.sportevent.R;
+import com.example.sportevent.data.model.entities.Event;
+
+import static com.example.sportevent.utilities.Constants.IMAGES;
+
+
+public class ClosestEvent extends Fragment implements View.OnClickListener{
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_closest_event, container, false);
+
+        // TODO: 05/01/2021 Get the first element in the list which has the closest event
+        ImageView imageView = view.findViewById(R.id.imageView);
+        Glide.with(this)
+                .asBitmap()
+                .load(IMAGES[0])
+                .into(imageView);
+
+        TextView textView1 = view.findViewById(R.id.textView1);
+        textView1.setText("TEST");
+
+        TextView textView2 = view.findViewById(R.id.textView2);
+        textView2.setText("TTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEESSSSSSSSSSSSSSSTTTTTTTTTTT");
+        textView2.setMovementMethod(new ScrollingMovementMethod());
+
+        final Button participantList = view.findViewById(R.id.participant_list);
+        participantList.setOnClickListener(this);
+        final Button startEvent = view.findViewById(R.id.event_signup);
+        startEvent.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        NavController navController = Navigation.findNavController(v);;
+        switch (v.getId()) {
+            case R.id.participant_list:
+                //  navController.navigate(R.id.action_eventDescriptionFragment1_to_participantListFragment1);
+                break;
+            case R.id.event_signup:
+                Toast.makeText(getContext(), "sign up window here", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+    }
+}
