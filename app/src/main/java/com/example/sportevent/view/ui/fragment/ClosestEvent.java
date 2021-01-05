8f41b1e108accc6933e92321a95f1078feeabf35
flere <s147199@student.dtu.dk>
2021-01-05 16:34:56 +0100

    Made closest event dialog popup

--- app/src/main/java/com/example/sportevent/view/ui/fragment/ClosestEvent.java
+++ app/src/main/java/com/example/sportevent/view/ui/fragment/ClosestEvent.java
@@ -1,6 +1,8 @@
 package com.example.sportevent.view.ui.fragment;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
@@ -9,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,23 +25,25 @@ import com.example.sportevent.utilities.SampleData;
 
 public class ClosestEvent extends Fragment implements View.OnClickListener{
 
+    private Event mEvent;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_closest_event, container, false);
 
         // TODO: 05/01/2021 Get the first element in the list which has the closest event
-        Event event = SampleData.signUpEventList.get(0);
+        mEvent = SampleData.signUpEventList.get(0);
         ImageView imageView = view.findViewById(R.id.imageView);
         Glide.with(this)
                 .asBitmap()
-                .load(event.getImageURL())
+                .load(mEvent.getImageURL())
                 .into(imageView);
 
         TextView textView1 = view.findViewById(R.id.textView1);
-        textView1.setText(event.getEventName());
+        textView1.setText(mEvent.getEventName());
 
         TextView textView2 = view.findViewById(R.id.textView2);
-        textView2.setText(event.getEventDescription());
+        textView2.setText(mEvent.getEventDescription());
         textView2.setMovementMethod(new ScrollingMovementMethod());
 
         final Button participantList = view.findViewById(R.id.participant_list);
@@ -50,7 +53,6 @@ public class ClosestEvent extends Fragment implements View.OnClickListener{
 
         return view;
     }
-
     @Override
     public void onClick(View v) {
         NavController navController = Navigation.findNavController(v);;
@@ -59,7 +61,25 @@ public class ClosestEvent extends Fragment implements View.OnClickListener{
                 //  navController.navigate(R.id.action_eventDescriptionFragment1_to_participantListFragment1);
                 break;
             case R.id.event_signup:
-                Toast.makeText(getContext(), "sign up window here", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext())
+                        .setTitle("Join The Event")
+                        .setMessage("Confirm for joining this event !")
+                        .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // TODO: 05/01/2021 Show an animation
+                                // TODO: 05/01/2021 Show toast message if the user is already sing up to the event OR find another solution
+                                SampleData.addJoinedEventList(mEvent);
+                                SampleData.removeSignUpEventList(mEvent); // TODO: 05/01/2021 the delete here should be from the data base
+
+                            }
+                        });
+                builder.create().show();
                 break;
 
             default:
