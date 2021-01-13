6b2b654b300076ae9245965971077471438460de
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-01-13 18:55:12 +0100

    - ClosestEvent fragment is working

--- app/src/main/java/com/example/sportevent/view/ui/fragment/ClosestEvent.java
+++ app/src/main/java/com/example/sportevent/view/ui/fragment/ClosestEvent.java
@@ -11,28 +11,36 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.bumptech.glide.Glide;
 import com.example.sportevent.R;
 import com.example.sportevent.data.model.entities.Event;
+import com.example.sportevent.utilities.Logic;
 import com.example.sportevent.utilities.SampleData;
+import com.example.sportevent.viewModel.EventViewModel;
 
 public class ClosestEvent extends Fragment implements View.OnClickListener{
-
+    public static final String TAG = "ClosestEvent";
     private Event mEvent;
+    private EventViewModel mEventViewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_closest_event, container, false);
 
+        mEventViewModel = new ViewModelProvider(this).get(EventViewModel.class);
+
         // TODO: 05/01/2021 Get the first element in the list which has the closest event
         mEvent = SampleData.signUpEventList.get(0);
+
         ImageView imageView = view.findViewById(R.id.imageView);
         Glide.with(this)
                 .asBitmap()
@@ -55,28 +63,29 @@ public class ClosestEvent extends Fragment implements View.OnClickListener{
     }
     @Override
     public void onClick(View v) {
-        NavController navController = Navigation.findNavController(v);;
+        NavController navController = Navigation.findNavController(v);
         switch (v.getId()) {
             case R.id.participant_list:
-                //  navController.navigate(R.id.action_eventDescriptionFragment1_to_participantListFragment1);
+                navController.navigate(ClosestEventDirections.actionClosestEventToParticipantListSignUpFragment(mEvent));
                 break;
             case R.id.event_signup:
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext())
                         .setTitle("Join The Event")
                         .setMessage("Confirm for joining this event !")
-                        .setNegativeButton("no", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
+                        .setNegativeButton("no", (dialog, which) -> {
 
-                            }
-                        }).setPositiveButton("yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // TODO: 05/01/2021 Show an animation
-                                // TODO: 05/01/2021 Show toast message if the user is already sing up to the event OR find another solution
+                        }).setPositiveButton("yes", (dialog, which) -> {
+                            // TODO: 05/01/2021 Show an animation until the firestore is being updated
+                            if (Logic.isUserAlreadySignUpToEvent(mEvent, SampleData.currentUserEmail)) {
+                                Toast.makeText(getContext(), "Already sign up", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getContext(), "Congratulations you just sign up", Toast.LENGTH_SHORT).show();
+                                mEvent.getJoinedEventParticipantsEmails().add(SampleData.currentUserEmail);
+                                mEventViewModel.createEvent(mEvent, mEvent.getId());
+                                mEventViewModel.getAllEvents().observe( this, requestCall -> {
+                                    SampleData.signUpEventList = requestCall.eventList;
+                                });
                                 SampleData.joinedEventList.add(mEvent);
-                                SampleData.signUpEventList.remove(mEvent); // TODO: 05/01/2021 the delete here should be from the data base
-
                             }
                         });
                 builder.create().show();
