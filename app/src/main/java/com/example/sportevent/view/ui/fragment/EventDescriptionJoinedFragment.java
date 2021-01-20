2773e77876e44219c085e94713a16c32087b7333
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-01-20 21:07:13 +0100

    Correction: check first if the date of race is finished or not started -> if not then ask for GPS Activation

--- app/src/main/java/com/example/sportevent/view/ui/fragment/EventDescriptionJoinedFragment.java
+++ app/src/main/java/com/example/sportevent/view/ui/fragment/EventDescriptionJoinedFragment.java
@@ -67,20 +67,19 @@ public class EventDescriptionJoinedFragment extends Fragment implements View.OnC
                 navController.navigate(EventDescriptionJoinedFragmentDirections.actionEventDescriptionFragmentToParticipant(mEvent));
                 break;
             case R.id.event_start_description:
-                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                } else {
                     Date currentDate = new Date();
-                    if(currentDate.after(mEvent.getRaceStartDate()) && currentDate.before(mEvent.getRaceEndDate()))
+                    if(currentDate.after(mEvent.getRaceStartDate()) && currentDate.before(mEvent.getRaceEndDate())) {
+                        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                        }
                         navController.navigate(EventDescriptionJoinedFragmentDirections.actionEventDescriptionFragmentToStartEvent(mEvent));
-                    else {
+                    } else {
                         if (currentDate.before(mEvent.getRaceStartDate())) {
                             makeAlertDialog("This event will start on " + mEvent.getRaceStartDate());
                         } else {
                             makeAlertDialog("This event has finished on " + mEvent.getRaceEndDate());
                         }
                     }
-                }
                 break;
             default:
                 break;
