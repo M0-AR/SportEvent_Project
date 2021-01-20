cd82d7090f3395631c6226fb6f70bec6e3ae7aa6
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-01-20 16:31:59 +0100

    Make TermAndCondition fragment

---
+++ app/src/main/java/com/example/sportevent/view/ui/TermAndCondition.java
@@ -0,0 +1,23 @@
+package com.example.sportevent.view.ui;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sportevent.R;
+
+public class TermAndCondition extends Fragment {
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.activity_term_and_condition, container, false);
+        return view;
+    }
+}
