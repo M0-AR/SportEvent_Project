3d8233bdc4e3cc274965a82cfd8de6731e2b0d09
Mohamad-Ashmar <s176492@student.dtu.dk>
2021-01-20 16:11:07 +0100

    Make about us fragment

---
+++ app/src/main/java/com/example/sportevent/view/ui/AboutUs.java
@@ -0,0 +1,30 @@
+package com.example.sportevent.view.ui;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.sportevent.R;
+import com.example.sportevent.view.ui.fragment.EventDescriptionSignUpFragmentArgs;
+
+public class AboutUs extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.activity_about_us, container, false);
+        return view;
+    }
+}
